7e56b32aa3a4845ecc508f674d05a713bdfed31a
Carlos Osuna <osunacj@gmail.com>
2021-11-29 14:13:56 -0600

    home

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,18 @@
 import Card from "react-bootstrap/Card";
 function Home() {
   return (
-    <Card>
-      <Card.Img variant="top" src="./bank.png" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <>
+      <div className="rounded bg-light p-2">
+        <h2 className="text-center ">Welcome to your Bank</h2>
+      </div>
+      <br />
+      <Card className="bg-light border-0">
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>This is the Home page for your Bank.</Card.Text>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 
